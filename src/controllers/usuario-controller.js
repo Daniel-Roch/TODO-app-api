55707dc8cd37d8dcc9489aceb649e26fc767b127
Daniel-Roch <daniel_roch4@hotmail.com>
2021-07-13 22:20:28 -0300

    Colocando um delete, e um find() para procurar por email

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,17 +1,33 @@
 module.exports = (app,db,User) =>{
-    app.get('/usuario', (req, res) => {
-        res.send(`Rota ativada com GET e recurso usuario:
-         valores de tarefas devem ser retornados`)
+    app.get('/usuario', (req, res, next) => {
+        let objTotal = db.users
+        if(db.users.length > 0){
+            res.json({objTotal,"count" : objTotal.length})
+            next()
+        }else{
+            res.json({'usuarios' : 0})
+        }
     })
     app.post('/usuario',(req,res)=>{
+        let count = db.users.length + 1
         const {nome,email,senha} = req.body
-        if(nome.length > 4 && email.includes('@') && senha.length > 4){
-            db.users.push(new User(nome,email,senha))
-            res.status(201).json(db.users)
-        }else{
-            res.status(400).send('[Erro] - Dados Inválidos')
-        }
+        db.users.push(new User(nome,email,senha))
+        res.status(201).json({'criacao':true, 'qtd': count})
         //Quero mostrar uma mensagem de estar ok
         //res.send(`Rota POST de usuario ativada: usuário adicionado ao banco de dados`)
+    })
+    //Procurando
+    app.get('/usuario/:email',(req,res)=>{
+        let filter = db.users.filter(users => req.params.email == users.email)
+        if(filter.length){
+            res.json({email : true, filter})
+        }else{
+            res.json({email : false})
+        }
+    })
+    //Deletando - ele cria um novo array e substituir o existente
+    app.delete('/usuario/:email',(req,res)=>{
+        db.users = db.users.filter(users => req.params.email != users.email)
+        res.json({'delete': true})
     })
 }
--- src/models/TasksModels.js
+++ src/models/TasksModels.js
@@ -1,12 +1,11 @@
 let idTasks = 0
 class Tarefas{
     constructor(titulo,descricao,status){
-        this.id = idTasks;
+        this.id = idTasks++;
         this.titulo = titulo;
         this.descricao = descricao;
         this.status = status;
         this.dataCriacao = new Date();
-        idTasks++
     }
 }
 
