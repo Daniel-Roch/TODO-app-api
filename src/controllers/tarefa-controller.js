5dbecb51a89409bb910b9ec1aa45818e98b8b134
Daniel-Roch <daniel_roch4@hotmail.com>
2021-07-14 23:15:50 -0300

    Adicionado a parte de modificação (put)

--- src/controllers/tarefa-controller.js
+++ src/controllers/tarefa-controller.js
@@ -1,14 +1,56 @@
 module.exports = (app,db,Tarefas) => {
     app.get('/tarefas', (req, res) => {
-        res.send(`Rota ativada com GET e recurso tarefas:
-        valores de tarefas devem ser retornados`)
+        let tasksAll = db.tasks
+        if(tasksAll.length > 0){
+            res.json({tasksAll,"count": tasksAll.length})
+        }else{
+            res.json({"tasks" : 0})
+        }
     })
+    //Adicionar
     app.post('/tarefas',(req,res)=>{
+        let count = db.tasks.length + 1
         const {titulo,descricao,status} = req.body
         db.tasks.push(new Tarefas(titulo,descricao,status))
-        res.status(201).json(db.tasks)
+        res.status(201).json({'criacao':true, 'qtd': count})
+    })
+    //Procurando
+    app.get('/tarefas/:titulo',(req,res)=>{
+        let filter = db.tasks.filter(tasks => req.params.titulo == tasks.titulo)
+        if(filter.length){
+            res.json({titulo : true, filter})
+        }else{
+            res.json({titulo : false})
+        }
+    })
+    //Deletando - ele cria um novo array e substituir o existente
+    app.delete('/tarefas/:titulo',(req,res)=>{
+        db.tasks = db.tasks.filter(tasks => req.params.titulo != tasks.titulo)
+        res.json({'delete': true})
+    })
 
-        //Quero mostrar uma mensagem de estar ok
-        //res.send(`Rota POST de terefas ativada: tarefas adicionado ao banco de dados`)
+     //alterar
+     app.put('/tarefas/:titulo',(req,res)=>{
+        db.tasks = db.tasks.map(tasks =>{
+            if(req.params.titulo == tasks.titulo){
+                if(req.body.titulo){
+                    res.json({'Alteração' : true})
+                    tasks.titulo = req.body.titulo
+                    return tasks
+                }else if(req.body.descricao){
+                    res.json({'Alteração' : true})
+                    tasks.descricao = req.body.descricao
+                    return tasks
+                }else if(req.body.status){
+                    res.json({'Alteração' : true})
+                    tasks.status = req.body.status
+                    return tasks
+                }
+                res.json({'Alteração' : false})
+                return tasks
+            }else{
+                return tasks
+            }
+        })
     })
 }
--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,21 +1,20 @@
 module.exports = (app,db,User) =>{
-    app.get('/usuario', (req, res, next) => {
+    app.get('/usuario', (req, res) => {
         let objTotal = db.users
         if(db.users.length > 0){
             res.json({objTotal,"count" : objTotal.length})
-            next()
         }else{
-            res.json({'usuarios' : 0})
+            res.json({'users' : 0})
         }
     })
+    //adiciona
     app.post('/usuario',(req,res)=>{
         let count = db.users.length + 1
         const {nome,email,senha} = req.body
         db.users.push(new User(nome,email,senha))
         res.status(201).json({'criacao':true, 'qtd': count})
-        //Quero mostrar uma mensagem de estar ok
-        //res.send(`Rota POST de usuario ativada: usuário adicionado ao banco de dados`)
     })
+    
     //Procurando
     app.get('/usuario/:email',(req,res)=>{
         let filter = db.users.filter(users => req.params.email == users.email)
@@ -29,5 +28,36 @@ module.exports = (app,db,User) =>{
     app.delete('/usuario/:email',(req,res)=>{
         db.users = db.users.filter(users => req.params.email != users.email)
         res.json({'delete': true})
+    })
+
+    //alterar
+    app.put('/usuario/:email',(req,res)=>{
+        //Vou verificar se o email existe!
+        let filter = db.users.filter(users => req.params.email == users.email)
+        if(filter.length){
+            db.users = db.users.map(users =>{
+                if(req.params.email == users.email){
+                    if(req.body.email){
+                        res.json({'Alteração' : true})
+                        users.email = req.body.email
+                        return users
+                    }else if(req.body.nome){
+                        res.json({'Alteração' : true})
+                        users.nome = req.body.nome
+                        return users
+                    }else if(req.body.senha){
+                        res.json({'Alteração' : true})
+                        users.senha = req.body.senha
+                        return users
+                    }
+                    res.json({'Alteração' : false})
+                    return users
+                }else{
+                    return users
+                }
+            })
+        }else{
+            res.json({email : false})
+        }
     })
 }
